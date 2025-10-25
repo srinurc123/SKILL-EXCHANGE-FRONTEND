4fea06497a2b6f6f0544daf28992e5f9eb7dbd80
srinu <srinuvalasani5@gmail.com>
2025-10-25 17:44:33 +0530

    all

--- src/api/apiClient.js
+++ src/api/apiClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: "http://localhost:8080/api",
+  baseURL: "https://skill-exchange-backend-production.up.railway.app/api",
   headers: {
     "Content-Type": "application/json",
   },
